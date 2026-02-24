c9be25e37380719c95f4560bd677bcfb93ab75e7
TomasBusquetsFrigola <a2409805@institutmontilivi.cat>
2026-02-24 17:25:44 +0100

    Inici Practica Viewmodels

---
+++ Practica-ViewModel/build.gradle.kts
@@ -0,0 +1,7 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.compose)
+    kotlin("plugin.serialization") version "2.0.21"
+}
+
